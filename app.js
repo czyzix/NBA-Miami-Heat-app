eb3c6db15098611fea9af82ba649b812cb5dbb88
czyzix <czyzevsky@gmail.com>
2023-02-08 12:11:21 +0100

    first work

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
+/* important links:
+'https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=79' - athlete stats
+`https://www.balldontlie.io/api/v1/players?search=butler` - search by lastname
+`http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/Miami/roster` - miami heat roster */
 
-fetch('https://www.balldontlie.io/api/v1/players?search=butler')
-	.then(response => response.json())
-	.then(response => console.log(response))
+import { renderAthletesList } from "./help-functions.js";
+
+let miamiHeatRoster = [];
+
+function getMiamiRoster() {
+	fetch('http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/Miami/roster')
+	.then(res => res.json())
+	.then((dataRaw) => {
+
+		miamiHeatRoster = dataRaw.athletes.map((athlete) => {
+			return {
+				id: athlete.id,
+				name: athlete.firstName,
+				lastName: athlete.lastName,
+				jersey: athlete.jersey,
+				position: athlete.position.name,
+				photoUrl: `https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/${athlete.id}.png&w=350&h=254`,
+			};
+		});
+		console.log(miamiHeatRoster);
+		renderAthletesList(miamiHeatRoster);
+	})
 	.catch(err => console.error(err));
+}
 
+getMiamiRoster();
 
-
-
-//'https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=79'
-//https://www.balldontlie.io/api/v1/players?search=butler
-
-
-//http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/Miami/roster - GIGA WAŻNE
---
+++ help-functions.js
@@ -0,0 +1,24 @@
+export const renderAthletesList = (miamiHeatRoster) => {
+    const rootElement = document.querySelector("#roster-container");
+    rootElement.innerHTML = "";
+    rootElement.appendChild(createAthletesListElement(miamiHeatRoster));
+};
+
+const createAthletesListElement = (miamiHeatRoster) => {
+    const listElement = document.createElement("ul");
+    miamiHeatRoster.forEach(athlete => {
+        listElement.appendChild(createAthleteCardElement(athlete));
+    });
+
+    return listElement;
+};
+
+const createAthleteCardElement = (athlete) => {
+    const athleteCardElem = document.createElement("li");
+
+    const athleteImg = document.createElement("img");
+    athleteImg.src = athlete.photoUrl;
+    athleteCardElem.appendChild(athleteImg);
+
+    return athleteCardElem;
+}
