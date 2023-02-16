4e9dcaf8a7d3a43e4d9ca5074b8c5061480e7f1c
czyzix <czyzevsky@gmail.com>
2023-02-16 13:28:56 +0100

    deleted unnecessary lines for fetching data

--- view-player.js
+++ view-player.js
@@ -3,6 +3,11 @@ import { renderPlayerCard } from "./help-functions.js";
 export const renderPlayerDetails = () => {
 
     let currentSeason;
+    let params = new URLSearchParams(window.location.search);
+    let statsId = params.get("athlete");
+    let name = params.get("name");
+    let lastname = params.get("lastname");
+    let playerInfo = [];
 
     // getting current NBA season year
     var date = new Date();
@@ -14,71 +19,7 @@ export const renderPlayerDetails = () => {
         currentSeason = new Date().getFullYear()
     };
 
-    let params = new URLSearchParams(window.location.search);
-    let statsId = params.get("athlete");
-    let name = params.get("name");
-    let lastname = params.get("lastname");
-
-    /* function getPlayerStats() {
-        fetch(`https://www.balldontlie.io/api/v1/season_averages?season=${currentSeason}&player_ids[]=${statsId}`)
-        .then(res => res.json())
-        .then((dataRaw) => {
-            playerStats = dataRaw.data.map((player) => {
-                return {
-                    gp: player.games_played,
-                    pts: player.pts,
-                    ast: player.ast,
-                    reb: player.reb,
-                    stl: player.stl,
-                    blk: player.blk,
-                    fg_pct: player.fg_pct
-                };
-            });
-            console.log(playerStats);
-        })
-        .catch(err => console.error(err));
-    }
-
-    function getPlayerInfo() {
-        fetch('http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/mia/roster')
-        .then(res => res.json())
-        .then((dataRaw) => {
-            miamiHeatRoster = dataRaw.athletes.map((athlete) => {
-                return {
-                    id: athlete.id,
-                    name: athlete.firstName,
-                    lastName: athlete.lastName,
-                    jersey: athlete.jersey,
-                    position: athlete.position.name,
-                    height: athlete.displayHeight.slice(0,4).replace(" ",""),
-                    weight: athlete.weight,
-                    age: athlete.age,
-                    experience: athlete.experience.years,
-                    photoUrl: `https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/${athlete.id}.png&w=350&h=254`,
-                };
-            });
-            filterForPlayer();
-        })
-        .then(renderPlayerCard(filteredPlayer))
-        .catch(err => console.error(err));
-    }
-
-    function filterForPlayer() {
-        filteredPlayer = miamiHeatRoster.filter((athlete) => {
-            return (
-                (athlete.name === name) && (athlete.lastName === lastname)
-            );
-        });
-        console.log(filteredPlayer);
-    };
-
-    getPlayerStats()
-    getPlayerInfo() */
-
-
-    let playerInfo = [];
-
-    const dupa = () => {
+    const getPlayerInfo = () => {
         Promise.all(
             [fetch(`https://www.balldontlie.io/api/v1/season_averages?season=${currentSeason}&player_ids[]=${statsId}`), 
             fetch('http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/mia/roster')])
@@ -101,12 +42,10 @@ export const renderPlayerDetails = () => {
                     lastName: values.lastName,
                     jersey: values.jersey,
                     position: values.position.name,
-                    height: values.displayHeight.slice(0,4).replace(" ",""),
+                    height: values.displayHeight.slice(0,4).replace(" ","")+'"',
                     weight: values.weight,
                     age: values.age,
-                    experience: values.experience.years,
                     photoUrl: `https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/${values.id}.png&w=350&h=254`,
-                    gp: values.games_played,
                     pts: values.pts,
                     ast: values.ast,
                     reb: values.reb,
@@ -116,14 +55,9 @@ export const renderPlayerDetails = () => {
                 }
                 
                 renderPlayerCard(playerInfo);
-
-                // const mappedObject = {
-                //     name: firstName,
-                //     age,
-                //     position: 
-                // }
+                console.log(playerInfo);
             })
     }
 
-    dupa();
+    getPlayerInfo();
 }
